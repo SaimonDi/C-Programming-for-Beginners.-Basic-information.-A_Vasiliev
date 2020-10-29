5a7157cf9b750d99c2e1ea3b88f71202c6d7d8c6
SaimonDi <alex-1992.04@mail.ru>
2020-10-29 06:40:08 +0300

    Добавлены шаблоны для заданий к Главе 1

---
+++ CSharp Programming for Beginners. Basic information. Alexey Vasiliev/Chapter_01/Task_5.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+/*
+Задача №5.
+
+Напишите программу, в которой по году рождения определяется возраст пользователя. 
+Используйте консольный ввод и вывод данных.
+*/
+
+namespace Task_5._1
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            Console.WriteLine("Hello World!");
+            }
+        }
+    }
